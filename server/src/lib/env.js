feaa6c2114c42ade2f266e7d9ba5f36a08a3cd29
bhamidipativiswajith2007 <viswajithbhamidipati@gmail.com>
2026-06-23 16:09:18 +0530

    deployment config

--- server/src/lib/env.js
+++ server/src/lib/env.js
@@ -5,4 +5,6 @@ export let ENV={
   //key value pairs
   PORT:process.env.PORT,
   DB_URL:process.env.DB_URL,
+  NODE_ENV:process.env.NODE_ENV,
+
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 import express from "express"
 // import dotenv from 'dotenv'
 // dotenv.config();//to accese the env file
-
+import path from 'path'
 import {ENV} from './lib/env.js'
 
 let app=express();
 
+let __dirname=path.resolve()
+
 app.get('/health',(re,res)=>{
 res.status(200).json({msg:"apii is healthy "})
 })
+
+//if app is ready for deploymrent
+if(ENV.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../client/dist")))//serving static files
+  
+  app.get('/{*any}',(re,res)=>{
+    res.sendFile(path.join(__dirname,"../client/dist/index.html"))
+  })
+}
 
 app.listen(3000,()=>console.log(`server ruuning on port 3000 ${ENV.PORT}`))
